22c5d6c5e0ece9187b2b5aff0e2216c416311e31
war5 <manofward@gmail.com>
2013-10-26 16:39:13 -0700

    added problem 18

---
+++ prob18.py
@@ -0,0 +1,11 @@
+list_1 = [3]
+list_2 = [7, 4]
+list_3 = [2, 6, 4]
+list_4 = [8, 5, 9, 3]
+master_list = []
+master_list.append(list_1)
+master_list.append(list_2)
+master_list.append(list_3)
+master_list.append(list_4)
+print master_list
+def find_path
